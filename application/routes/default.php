fd230b31118340715db8b875e8056740568f076f
Robbie Mackay <rm@robbiemackay.com>
2015-06-08 21:12:08 +1200

    Add savedsearches api route

--- application/routes/default.php
+++ application/routes/default.php
@@ -162,6 +162,19 @@ Route::set('dataproviders-api', 'api/v2/dataproviders(/<id>)',
 	));
 
 /**
+ * SavedSearches API Route
+ */
+Route::set('savedsearches-api', 'api/v2/savedsearches(/<id>)',
+	array(
+		'id' => '[a-zA-Z_-]+'
+	))
+	->defaults(array(
+		'action'     => 'index',
+		'directory'  => 'Api',
+		'controller' => 'DataProviders',
+	));
+
+/**
  * Post stats API route
  */
 Route::set('post-stats-api', 'api/v2/stats/posts')
